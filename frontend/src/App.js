6df53e99c9bf2b0d17ac86512a1d2e35f2533d55
paying4529 <b07901102@ntu.edu.tw>
2020-12-09 21:52:34 +0800

    page router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,11 @@ import './App.css';
 import { clickToGet, newuser, userlogin } from './axios'
 import React, { useState } from 'react'
 import Select from "react-select"
-
-function App() {
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Button, List, ListItem} from '@material-ui/core';
+function Register(){
   const [clicked, setClick] = useState(false)
   const [message, setMsg] = useState("")
-  const [loginSuccess, setLogin] = useState(false)
-  const [registerSuccess, setSuccess] = useState(false)
   const [username, setName] = useState("")
   const [password, setpwd] = useState("")  
   const [userclass, setClass] = useState("") 
@@ -18,6 +17,7 @@ function App() {
   ]
   return (
     <div className="App">
+      <h2>Register</h2>
       <p>Username: </p>
       <input value={username}
             onChange={(e) => setName(e.target.value)}></input>
@@ -25,49 +25,92 @@ function App() {
       <input value={password}
             onChange={(e) => setpwd(e.target.value)}></input>
       <p>Class: </p>
-      {/* <input value={userclass}
-            onChange={(e) => setClass(e.target.value)}></input> */}
       <Select value={userclass}
               onChange={(e) => {
                 setClass(e)
               }}
               options={classoptions}/>
-      <button onClick={async () => {
+      <Button variant="contained"
+        onClick={async () => {
           setClick(true)
           const userinfo = { username: username,
                              password: password,
                              userclass: userclass.value     }
           let msg = await newuser(userinfo)
           setMsg(msg)
-          if(msg.split(" ")[0] === 'new'){
-            setSuccess(true)
-          }
         }}
-        disabled={!userclass}>Register</button>
-      <button onClick={async () => {
-          setClick(true)
-          const userinfo = { username: username,
-                             password: password  }
-          let msg = await userlogin(userinfo)
-          setMsg(msg)
-          if(msg.split(" ")[0] === 'correct'){
-            setLogin(true)
-          }
-        }}
-        disabled={!password}>Login</button>
-      <button onClick={async () => {
-          setLogin(false)
-        }}
-        disabled={!loginSuccess}>Logout</button>
-      <button onClick={async () => {
-          setClick(true)
-          let msg = await clickToGet()
-          setMsg(msg)
-      }}>get list</button>
+        disabled={!userclass}>Register</Button>
+      {clicked? <div>{message}</div>:<></>}
+    </div>
+  )
+}
 
+function Login(){
+  const [clicked, setClick] = useState(false)
+  const [message, setMsg] = useState("")
+  const [loginSuccess, setLogin] = useState(false)
+  const [username, setName] = useState("")
+  const [password, setpwd] = useState("")  
+  return (
+    <div className="App">
+      <h2>Login</h2>
+      <p>Username: </p>
+      <input value={username}
+             onChange={(e) => setName(e.target.value)}></input>
+      <p>Password: </p>
+      <input value={password}
+             onChange={(e) => setpwd(e.target.value)}></input>
+      <Button variant="contained" 
+        onClick={async () => {
+            setClick(true)
+            const userinfo = { username: username, password: password  }
+            let msg = await userlogin(userinfo)
+            setMsg(msg)
+            if(msg.split(" ")[0] === 'correct'){ setLogin(true) }
+          }}
+        disabled={!password}>Login</Button>
+      <Button variant="contained" 
+        onClick={async () => { setLogin(false) }}
+        disabled={!loginSuccess}>Logout</Button>
       {clicked? <div>{message}</div>:<></>}
       {loginSuccess?<div>Hi, {username}</div>:<div>not login</div>}
     </div>
+  )
+}
+
+function Home(){
+  const [clicked, setClick] = useState(false)
+  const [message, setMsg] = useState("")
+  return (
+    <div className="App">
+      <h2>Home</h2>
+      <Button variant="contained"
+        onClick={async () => {
+          setClick(true)
+          let msg = await clickToGet()
+          setMsg(msg)
+        }}>get list</Button>
+      {clicked? <div>{message}</div>:<></>}
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <List component="nav">
+          <ListItem><Link to="/">Home</Link></ListItem>
+          <ListItem><Link to="/register">Register</Link></ListItem>
+          <ListItem><Link to="/login">Login</Link></ListItem>
+        </List>
+        <Switch>
+          <Route path="/register"><Register /></Route>
+          <Route path="/login"><Login /></Route>
+          <Route path="/"><Home /></Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
