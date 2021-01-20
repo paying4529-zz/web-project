5e850082588caf4c11d1a664ec719f5c9791e2aa
paying4529 <b07901102@ntu.edu.tw>
2021-01-21 02:17:30 +0800

    fd

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -10,12 +10,36 @@ function Home(){
     
     return (
       <div className="Home_page">
-        <h2>Home</h2>
-        <div class="button">
+        <h2>Instruction</h2>
+        <h3>This is a Hierarchy management app.</h3>
+        <div className="ins">
+          <h3>If you are general director, you can...</h3>
+          <h4>&emsp;Set the end date</h4>
+          <h4>&emsp;Manage group organization</h4>
+          <h4>&emsp;Manage your calander</h4>
+          <h4>&emsp;Assign Todo to members</h4>
+          <h4>&emsp;Import Todo to calander</h4>
+          <h4>&emsp;Modify the flowchart</h4>
+        </div>
+        <div className="ins">
+          <h3>If you are group manager, you can...</h3>
+          <h4>&emsp;Get the end date</h4>
+          <h4>&emsp;Assign Todo to members</h4>
+          <h4>&emsp;Manage your calander</h4>
+          <h4>&emsp;Modify the flowchart</h4>
+        </div>
+        <div className="ins">
+          <h3>If you are group member, you can...</h3>
+          <h4>&emsp;Get the end date</h4>
+          <h4>&emsp;Manage your calander</h4>
+          <h4>&emsp;Receive Todo</h4>
+          <h4>&emsp;Read the flowchart</h4>
+        </div>
+        {/* <div class="button">
         <Button variant="contained"
           onClick={()=>{setClick(true)}}>
           get list</Button></div>
-        {clicked? <div>{data.getUsers.map(e => (<div>{`${e.username}||${e.password}||${e.userclass}`}</div>))}</div>:<></>}
+        {clicked? <div>{data.getUsers.map(e => (<div>{`${e.username}||${e.password}||${e.userclass}`}</div>))}</div>:<></>} */}
       </div>
     )
   }
